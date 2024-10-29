341ba3057851fca10ac762a8b8b5ef382dcd8ed5
Shahmeer-create <malikshahmmer@gmail.com>
2024-10-29 05:12:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World I AM Testing!"
+    return "Hello, World I AM Testing all the best !"
 
 @app.route('/greet/<name>')
 def greet(name):
