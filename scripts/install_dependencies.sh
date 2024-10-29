ae3eb866be33f50e0c39c9fedd824cb8efccd6f2
Shahmeer-create <malikshahmmer@gmail.com>
2024-10-29 02:28:50 -0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/myapp
 source venv/bin/activate
+pip install flask 
 pip install -r requirements.txt
 sudo systemctl restart myapp
 chmod +x scripts/install_dependencies.sh
